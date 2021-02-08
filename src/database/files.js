fc6d3a37cc0b1f85c54e3375d4b12ba0ac58b5d1
Naveen-Bhati <bhatin626@gmail.com>
2021-02-08 07:29:07 +0530

    second commit of dynamis fileupload task

--- src/database/files.js
+++ src/database/files.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+//connecting database with database name "files_Details"
 mongoose
   .connect("mongodb://localhost:27017/files_Details", {
     useNewUrlParser: true,
@@ -13,11 +14,14 @@ mongoose
     console.log(err);
   });
 
+//creating a schema for uploading filename and it's size of type string
+
 const fileSchema = new mongoose.Schema({
   uploadedFileName: String,
   fileSize: String,
 });
 
+//creating a new collection "FileDetail"  which will be created as "filesdetails"
 const fileDetail = new mongoose.model("FileDetail", fileSchema);
 
 module.exports = fileDetail;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-const { static, urlencoded } = require("express");
 const express = require("express");
 const http = require("http");
 const path = require("path");
-const upload = require("express-fileupload");
+const upload = require("express-fileupload"); //requiring "express-fileupload dependency for uploading file into directory"
 const port = process.env.port || 3000;
 const app = express();
-const fileDetail = require("./database/files");
-app.use(express.urlencoded({ extended: false }));
+const fileDetail = require("./database/files"); //importing filedetails from mongoose js file
 const server = http.createServer(app);
 const publicDirPath = path.join(__dirname, "../public/index.html");
 app.use(upload());
+app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.sendFile(publicDirPath);
 });
+
+//uploading file with express-upload
 app.post("/file", async (req, res) => {
   if (req.files) {
     var file = req.files.uploadFile;
@@ -24,7 +25,7 @@ app.post("/file", async (req, res) => {
         res.send(err);
       } else {
         res.send("<h1>File Uploaded Successfully </h1>");
-
+        // saving the filaname and size to the mongoDB files_Details database
         try {
           const mongo = new fileDetail({
             uploadedFileName: fileName,
